bed8cbb07f25020295877db6e3258566fc868bfb
Elena Arocena <elena.arocena@innoway.es>
2023-10-16 12:19:40 +0200

    feat(heroes): create a new component heroes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './heroes/heroes.component';
+import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    CounterComponent
   ],
   imports: [
     BrowserModule
